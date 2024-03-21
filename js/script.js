c63cecc495b164ec94ba4893f041cbe69ff3a1ee
lai2-24 <lai2-24@itismeucci.com>
2024-03-21 13:31:01 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let map = L.map('map').setView([43.7695604, 	11.2558136], 15);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
